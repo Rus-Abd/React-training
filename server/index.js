898081007c85ada4c7a204899fec9bcd77546355
ruslan <ruslan.abdallah2000@gmail.com>
2022-01-30 19:24:26 +0300

    init/jwt-auth: init starter files

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`Server started at PORT = ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
